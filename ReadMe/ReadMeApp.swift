71032b573bd196de1b5f9146fb6bdbf8660757f6
Shubham Naik <shub@shub.club>
2021-02-01 21:40:25 -0800

    Initial Commit

---
+++ ReadMe/ReadMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReadMeApp.swift
+//  ReadMe
+//
+//  Created by Shubham Naik on 2/1/21.
+//
+
+import SwiftUI
+
+@main
+struct ReadMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
